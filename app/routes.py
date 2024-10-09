02754da0024594dcb11f1e2790a6087ab9ee7f15
Avery <averymaxwell33@gmail.com>
2024-10-08 20:43:11 -0400

    send

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,19 @@ from . import app
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/runners')
+def runners():
+    return render_template('runners.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
