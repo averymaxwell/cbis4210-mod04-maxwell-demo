8101655279f4fa4d9efcc5b511515f18a32742ec
Avery <averymaxwell33@gmail.com>
2024-10-11 14:59:45 -0400

    send

--- app/db_connect.py
+++ app/db_connect.py
@@ -3,22 +3,22 @@ import pymysql.cursors
 from flask import g
 
 def get_db():
+    # Check if the database connection is not in `g` or if it's closed
     if 'db' not in g or not is_connection_open(g.db):
         print("Re-establishing closed database connection.")
         g.db = pymysql.connect(
-            # Database configuration
-            # Configure MySQL
-            host = 'ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-            user = 'omlftffrjeogfyiz',
-            password = 'v5koskzhmheuiihh',
-            database = 'adyqrjdspobi3hy2',
-            cursorclass=pymysql.cursors.DictCursor  # Set the default cursor class to DictCursor
+            # Updated Database Configuration
+            host='vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',  # Updated host
+            user='hsd0p4mpaegonjkl',       # Updated user
+            password='fm83hnwngpgij1j3',    # Updated password
+            database='as9wdavrfnsianhl',    # Updated database name
+            cursorclass=pymysql.cursors.DictCursor  # Use DictCursor for dictionary-based results
         )
     return g.db
 
 def is_connection_open(conn):
     try:
-        conn.ping(reconnect=True)  # PyMySQL's way to check connection health
+        conn.ping(reconnect=True)  # Check connection health and reconnect if needed
         return True
     except:
         return False
@@ -28,6 +28,3 @@ def close_db(exception=None):
     if db is not None and not db._closed:
         print("Closing database connection.")
         db.close()
-
-
-
